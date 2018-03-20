b872d635e7a65317fccdaed329d3e7494a301588
raisbilel <37501198+raisbilel@users.noreply.github.com>
2018-03-20 12:37:34 +0100

    Update mymovies.py
    Added sample movies.

--- mymovies.py
+++ mymovies.py
@@ -3,7 +3,16 @@ import fresh_tomatoes
 
 movies = []   # Initiates the movies containing list
 
-# Define your movies here!
+inception = media.Movie(
+	'Inception',
+	'https://i.ytimg.com/vi/E1iqGiX0lSg/movieposter.jpg',
+	'https://www.youtube.com/watch?v=8hP9D6kZseM' )
+movies.append(inception)  # Appends my new movie the movies list
 
+catch = media.Movie(
+	'Catch Me If You Can',
+	'https://images-na.ssl-images-amazon.com/images/I/51bPnAto--L.jpg',
+	'https://www.youtube.com/watch?v=SosRcIMCr5g')
+movies.append(catch)  # Appends my new movie the movies list
 
 fresh_tomatoes.open_movies_page(movies)
